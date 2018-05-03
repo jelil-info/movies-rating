2c46ff4c93f851d294dd4fc141e81cec3f4de2a0
Aneeta Sharma <get.aneeta@gmail.com>
2018-05-04 00:06:29 +0545

    Setup facebook strategy.

--- config/Config.js
+++ config/Config.js
@@ -1,4 +1,6 @@
 module.exports = {
   DB: 'mongodb://localhost/movie_rating_app',
   SECRET: 'movieratingappsecretkey',
+  FACEBOOK_APP_ID: '161708997950642',
+  FACEBOOK_APP_SECRET: '256ed3ac3bafacfc00ed7206a7cb93c3',
 }
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,29 @@
 <template>
-  <v-form v-model="valid" ref="form" lazy-validation>
-    <v-text-field
-      label="Email"
-      v-model="email"
-      :rules="emailRules"
-      required
-    ></v-text-field>
-    <v-text-field
-      label="Password"
-      v-model="password"
-      required
-    ></v-text-field>
-    <v-btn
-      @click="submit"
-      :disabled="!valid"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">clear</v-btn>
-  </v-form>
+  <div>
+    <div class="login">
+       <a href="/login/facebook"> Login with Facebook</a>
+    </div>
+    <v-form v-model="valid" ref="form" lazy-validation>
+      <v-text-field
+        label="Email"
+        v-model="email"
+        :rules="emailRules"
+        required
+      ></v-text-field>
+      <v-text-field
+        label="Password"
+        v-model="password"
+        required
+      ></v-text-field>
+      <v-btn
+        @click="submit"
+        :disabled="!valid"
+      >
+        submit
+      </v-btn>
+      <v-btn @click="clear">clear</v-btn>
+    </v-form>
+  </div>
 </template>
 <script>
 import axios from 'axios';
